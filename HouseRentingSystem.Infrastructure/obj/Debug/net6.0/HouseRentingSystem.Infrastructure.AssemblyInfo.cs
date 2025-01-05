6c1f57831d99ad012996a432565fa453a53c23b5
martinpakev <marti_pakev@abv.bg>
2025-01-05 17:45:35 +0200

    Repository changed and project is fixed.

--- HouseRentingSystem.Infrastructure/obj/Debug/net6.0/HouseRentingSystem.Infrastructure.AssemblyInfo.cs
+++ HouseRentingSystem.Infrastructure/obj/Debug/net6.0/HouseRentingSystem.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HouseRentingSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66798bd4d0c973b69cab79ff8c15a1009b1ede7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HouseRentingSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HouseRentingSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
