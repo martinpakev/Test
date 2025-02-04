0870132429d3e35a9fe5ecdeae7c0beb3493a519
martinpakev <marti_pakev@abv.bg>
2025-02-04 22:31:52 +0200

    Login and Register pages edited

--- HouseRentingSystem/Program.cs
+++ HouseRentingSystem/Program.cs
@@ -1,7 +1,12 @@
 using HouseRentingSystem.ModelBinders;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using HouseRentingSystem.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("HouseRentingDbContextConnection") ?? throw new InvalidOperationException("Connection string 'HouseRentingDbContextConnection' not found.");
+
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationIdentity(builder.Configuration);
